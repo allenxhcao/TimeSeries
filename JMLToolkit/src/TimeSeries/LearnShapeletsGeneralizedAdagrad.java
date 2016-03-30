ad31e0973920880cd3a19e495b7106fe5499f95f
allenxhcao <allenxhcao@gmail.com>
2016-03-30 14:10:48 -0400

    Tasla Update

--- JMLToolkit/src/TimeSeries/LearnShapeletsGeneralizedAdagrad.java
+++ JMLToolkit/src/TimeSeries/LearnShapeletsGeneralizedAdagrad.java
@@ -656,11 +656,10 @@ public class LearnShapeletsGeneralizedAdagrad
 		if (args.length == 0) {
 			//String dir = "E:\\Data\\classification\\timeseries\\",
 			String dir = "UCR_TS_Archive_2015\\",
-<<<<<<< HEAD
+
 			ds = "TwoLeadECG"; 
-=======
+
 			ds = "FaceFour"; 
->>>>>>> origin/master
 
 			String sp = File.separator; 
 		
@@ -672,27 +671,21 @@ public class LearnShapeletsGeneralizedAdagrad
  				
 				"alpha=-50",
 				"eta=0.1",
-<<<<<<< HEAD
 				"maxEpochs=1000",
 				"K=-1",		
 				"L=0.2", 
 				"R=2", 
-=======
 				"maxEpochs=600",
 				"K=-1",		
 				"L=0.1", 
 				"R=3", 
->>>>>>> origin/master
 				"lambdaW=0.01" 
 				};
 			
 		}	
 		String dir = "UCR_TS_Archive_2015\\",
-<<<<<<< HEAD
 				ds = "TwoLeadECG"; 
-=======
 				ds = "FaceFour"; 
->>>>>>> origin/master
 		String sp = File.separator; 
 
 		// values of hyperparameters
@@ -821,11 +814,8 @@ public class LearnShapeletsGeneralizedAdagrad
 				+ "time="+ (endTime-startTime) 
 				); 
 		writer.close();
-<<<<<<< HEAD
 		// lsg.PrintShapeletsAndWeights();
-=======
 		lsg.PrintShapeletsAndWeights();
->>>>>>> origin/master
 		lsg.SaveShapeletsToFile(dir + ds + sp    
 				+ ds + "_LearnedShapelets");
 		
